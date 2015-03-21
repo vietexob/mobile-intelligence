6bcf286bb5271aeb7a40da93ec1696b08a6961c0
Viet Le Truc <vietexob@gmail.com>
2015-03-21 01:09:33 -0400

    Spatial in progress

--- code/spatial/descriptive_stats.R
+++ code/spatial/descriptive_stats.R
@@ -14,6 +14,29 @@ library(plyr)
 ## (3) Download and install 'rgeos' from CRAN: R CMD INSTALL ./rgeos_0.3-8.tar
 ## (4) http://spatial.ly/2010/11/installing-rgdal-on-mac-os-x/
 
-## 1. Loading Spatial Data
+## 1. Read the shapefiles
 filepath <- "./data/sz_shp/"
-sz_bou <- readOGR(dsn = filepath, layer = "sz_bou")
+sz_bou <- readOGR(dsn=filepath, layer="sz_bou")
+sz_road <- readOGR(dsn=filepath, layer="sz_road")
+sz_veg <- readOGR(dsn=filepath, layer="sz_veg")
+sz_poi <- readOGR(dsn=filepath, layer="sz_poi")
+sz_tow <- readOGR(dsn=filepath, layer="sz_tow")
+sz_wat <- readOGR(dsn=filepath, layer="sz_wat")
+
+## What is going on here?
+proj4string(sz_bou) <- CRS("+init=epsg:27700")
+
+## Convert shapefiles into data frames
+sz_bou.data <- fortify(sz_bou)
+sz_road.data <- fortify(sz_road)
+sz_veg.data <- fortify(sz_veg)
+sz_poi.data <- fortify(sz_poi)
+sz_tow.data <- fortify(sz_tow)
+sz_wat.data <- fortify(sz_wat)
+
+
+
+
+
+
+
