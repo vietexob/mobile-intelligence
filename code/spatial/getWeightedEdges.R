da345f26c6004e52cbe34bdf49f6f645db5e20d2
Joe Le Truc <vietexob@gmail.com>
2015-04-07 16:08:02 -0400

    Corrected spatial call graph visualization

--- code/spatial/getWeightedEdges.R
+++ code/spatial/getWeightedEdges.R
@@ -4,14 +4,10 @@ getWeightedEdges <- function(call.data, lower.bound=0, upper.bound=50) {
   
   ## Select the callers that are also callees
   call_ids <- intersect(call.data$caller_id, call.data$callee_id)
-  ## TODO: Should reconsider this criteria to reduce data loss
   subset.call.data <- subset(call.data, caller_id %in% call_ids)
   subset.call.data <- subset(subset.call.data, callee_id %in% call_ids)
   
-  ## Associate each caller with their **most frequented** location
-  ## This is because each agent (caller/callee) is mobile, thus their location
-  ## keeps changing throughout the duration, and we can only plot one of those
-  ## during the duration.
+  ## Associate each caller with their **vector** of locations.
   caller_id.cell_id <- new.env()
   for(a_call_id in call_ids) {
     subset.caller_id <- subset(subset.call.data, caller_id == a_call_id)
@@ -19,12 +15,6 @@ getWeightedEdges <- function(call.data, lower.bound=0, upper.bound=50) {
       ## Find the most frequented location
       cell_id.table <- table(subset.caller_id$cell_id)
       cell_id.table <- subset(cell_id.table, cell_id.table > 0)
-#       print(length(table(cell_id.table)))
-#       most.freq.cell_id <- names(sort(cell_id.table, decreasing=TRUE))[1]
-      if(length(cell_id.table) > 1) {
-        print(a_call_id)
-        print(names(cell_id.table))
-      }
       caller_id.cell_id[[toString(a_call_id)]] <- names(cell_id.table)
     }
   }
@@ -37,50 +27,57 @@ getWeightedEdges <- function(call.data, lower.bound=0, upper.bound=50) {
   weightedEdges <- subset(weightedEdges, Freq > lower.bound)
   weightedEdges <- subset(weightedEdges, Freq < upper.bound)
   
-  ## Map each agent to a pair of spatial coordinates
+  ## Map each agent to all of their spatial coordinates during the duration
   caller_location <- vector()
   callee_location <- vector()
-  caller_locs <- vector()
-  callee_locs <- vector()
+  freq <- vector()
   for(i in 1:nrow(weightedEdges)) {
+    a_freq <- weightedEdges$Freq[i]
     caller_id <- toString(weightedEdges$caller_id[i])
     caller_loc <- caller_id.cell_id[[caller_id]]
-    if(is.null(caller_loc)) {
-      caller_location[i] <- NA
-    } else {
-      if(length(caller_loc) == 1) {
-        caller_location[i] <- caller_loc
-      } else {
-        caller_locs <- caller_loc
-      }
-    }
-    
     callee_id <- toString(weightedEdges$callee_id[i])
     callee_loc <- caller_id.cell_id[[callee_id]]
-    if(is.null(callee_loc)) {
-      callee_location[i] <- NA
-    } else {
-      if(length(callee_loc) == 1) {
-        callee_location[i] <- callee_loc
-      } else {
-        callee_locs <- callee_loc
-      }
-    }
     
-    if(length(caller_locs) > 0 || length(callee_locs) > 0) {
-      if(length(caller_locs) > 0 && length(callee_locs) > 0) {
+    if(!is.null(caller_loc) && !is.null(callee_loc)) {
+      if(length(caller_loc) == 1 && length(callee_loc) == 1) {
+        ## Create an edge between the pair of locations
+        caller_location <- c(caller_location, caller_loc)
+        callee_location <- c(callee_location, callee_loc)
+        freq <- c(freq, a_freq)
+      } else { # at least one has multiple locations
+        n_edges <-length(caller_loc) * length(callee_loc)
+        a_freq <- ceiling(a_freq / n_edges)
+        freq <- c(freq, rep(a_freq, n_edges))
         
-      } else {
-        if(length(caller_locs) > 0) {
-          
+        if(length(caller_loc) > 1 && length(callee_loc) > 1) {
+          ## Create complete bipartite graph connecting the locations of callers
+          ## to the locations of callees.
+          for(j in 1:length(caller_loc)) {
+            for(k in 1:length(callee_loc)) {
+              caller_location <- c(caller_location, caller_loc[j])
+              callee_location <- c(callee_location, callee_loc[k])
+            }
+          }
         } else {
-          print("This will never happen?")
+          ## Either caller or callee has only one location (and the other has multiple)
+          if(length(caller_loc) > 1) {
+            for(j in 1:length(caller_loc)) {
+              caller_location <- c(caller_location, caller_loc[j])
+            }
+            callee_location <- c(callee_location, rep(callee_loc, length(caller_loc)))
+          } else { # length(callee_loc) > 1
+            caller_location <- c(caller_location, rep(caller_loc, length(callee_loc)))
+            for(j in 1:length(callee_loc)) {
+              callee_location <- c(callee_location, callee_loc[j])
+            }
+          }
         }
       }
     }
   }
   
-  ## Combine the attributes into a data frame and return
-  weightedEdges <- cbind(weightedEdges, caller_location, callee_location)
+  ## Combine the attributes into a **new** data frame and return
+  weightedEdges <- data.frame(caller_loc = caller_location, callee_loc = callee_location,
+                              freq = freq)
   return(weightedEdges)
 }
--- code/spatial/visualize_mobile_data.R
+++ code/spatial/visualize_mobile_data.R
@@ -19,7 +19,7 @@ rm(list = ls())
 ## Load the required packages, assuming they have been installed
 library(rmongodb)
 library(ggplot2)
-# library(ggmap) # for plotting maps
+library(ggmap) # for plotting maps
 library(igraph) # for plotting graphs
 library(popgraph) # for plotting population graphs
 library(scales) # for plot formatting 
@@ -45,7 +45,7 @@ load("./data/mobile/cell_id_rowIndex_mapping.RData")
 ## Because it has taken a significant retrieval time, I saved it as an offline file
 ## and load it whenever I want to use it. If you want to use this data frame, skip lines
 ## 67 to 155 to save time. Feel free to experiment with other params and get a diff dataset.
-# call.data <- read.csv(file="./data/mobile/my_call_data.csv")
+call.data <- read.csv(file="./data/mobile/my_call_data_100_300_1500.csv")
 
 ## Login credentials
 host <- "heinz-tjle.heinz.cmu.edu"
@@ -92,15 +92,15 @@ dimei.distr <- as.data.frame(mimei.distr) # convert matrix into data frame
 colnames(dimei.distr) <- c("imei", "freq") # name the columns
 dimei.distr$freq <- call.freq
 
-# ## Plot the histogram of the IMEI frequencies
-# (ggplot(dimei.distr, aes(freq)) + geom_histogram(binwidth=2, fill="#c0392b", alpha=0.75) +
-#    fivethirtyeight_theme() + 
-#    labs(title="Distribution of Call Frequencies in [80, 100]",
-#         x="Call Frequency", y="Frequency") + scale_x_continuous(labels=comma) +
-#    scale_y_continuous(labels=comma) + geom_hline(yintercept=0, size=0.4, color="black"))
-# 
-# ## Save the plot to disk
-# ggsave(file="./figures/mobile/call_freq.png", width=4, height=3)
+## Plot the histogram of the IMEI frequencies
+(ggplot(dimei.distr, aes(freq)) + geom_histogram(binwidth=2, fill="#c0392b", alpha=0.75) +
+   fivethirtyeight_theme() + 
+   labs(title="Distribution of Call Frequencies in [80, 100]",
+        x="Call Frequency", y="Frequency") + scale_x_continuous(labels=comma) +
+   scale_y_continuous(labels=comma) + geom_hline(yintercept=0, size=0.4, color="black"))
+
+## Save the plot to disk
+ggsave(file="./figures/mobile/call_freq.png", width=4, height=3)
 
 ## Sort the data frame by frequency in decreasing order
 dimei.distr <- dimei.distr[order(-dimei.distr$freq), ]
@@ -162,143 +162,145 @@ mongo.cursor.destroy(cursor)
 mongo.disconnect(mongo)
 mongo.destroy(mongo)
 
-# ## Match each cell_id with its corresponding rowIndex of the coordinate table
-# progress.bar <- create_progress_bar("text")
-# progress.bar$init(nrow(call.data))
-# rowIndices <- vector() # a vector of row indices
-# nMatches <- 0 # count the number of matches
-# for(i in 1:nrow(call.data)) {
-#   ## Have to convert numeric to string in order to do lookup
-#   cell_id <- toString(call.data$cell_id[i])
-#   if(nchar(cell_id) > 0) {
-#     ## Look up the (loaded) table
-#     rowIndex <- cell_id.coord.rowIndex[[cell_id]]
-#     if(is.null(rowIndex)) { # if matched
-#       rowIndices <- c(rowIndices, NA)
-#     } else { # if unmatched
-#       rowIndices <- c(rowIndices, rowIndex)
-#       nMatches <- nMatches + 1
-#     }
-#   } else {
-#     rowIndices <- c(rowIndices, NA)
-#   }
-#   
-#   progress.bar$step()
-# }
-# ## Calculate the matched percentage
-# match.pct <- round(nMatches / nrow(call.data) * 100, 2)
-# print(paste("Percent matched locations =", match.pct))
-# 
-# ## Reduce call data to those that can be plotted on a map
-# my.call.data <- call.data[!is.na(rowIndices), ]
-# ## Further reduce to include a certain date range only. Why?
-# (table(my.call.data$date))
-# my.call.data <- subset(my.call.data, date > 20080229)
-# my.call.data <- subset(my.call.data, date < 20080308)
-# 
-# ## Aggregate all the call records by date and find the locations of the cell towers
-# ## and the frequency of the calls made from each.
-# aggregate.location <- aggregateLocationByDate(my.call.data,
-#                                               cell_id.coord.rowIndex, cell.towers)
-# 
-# ## Create a spatial bubble chart
-# ## First, need a location at the center of all cell towers in my call data
-# ## Calculate the mean longitude
-# all.longitude <- names(table(aggregate.location$longitude))
-# all.longitude <- as.numeric(all.longitude)
-# mean.longitude <- mean(all.longitude)
-# 
-# ## Calculate the mean latitude
-# all.latitude <- names(table(aggregate.location$latitude))
-# all.latitude <- as.numeric(all.latitude)
-# mean.latitude <- mean(all.latitude)
-# 
-# ## Retrieved a map centered at the given location
-# location <- c(mean.longitude, mean.latitude)
-# location.map <- get_map(location, maptype = "terrain", zoom = 10, scale=2)
-# location.map <- ggmap(location.map, extent = 'device', legend = 'none')
-# ## Add the "bubbles" to the map, and size them by frequency.
-# ## Here we choose shape 21 (circle) and fill it red. For a full list of shapes,
-# ## see http://sape.inf.usi.ch/quick-reference/ggplot2/shape
-# location.map <- location.map + geom_point(data = aggregate.location,
-#                                           aes(x = longitude, y = latitude, size = freq),
-#                                           fill="red", alpha=0.80, shape=21)
-# ## Remove any legends from the plot
-# location.map <- location.map + guides(fill=FALSE, alpha=FALSE, size=FALSE)
-# ## Add a title to the plot
-# location.map <- location.map + ggtitle("Distribution of Call Events over the Week")
-# ## Add a fancy theme to it (this step is optional)
-# location.map <- location.map + fivethirtyeight_theme()
-# ## Split the plot into tiles (or facets), one for each date
-# location.map <- location.map + facet_wrap(~date)
-# ## Display the plot (or else it won't show)
-# print(location.map)
-# 
-# ## Save the plot on disk
-# ggsave(filename="./figures/mobile/my_call_bubbles.png", width=10, height=10)
-# 
-# ## Here, we plot the population graph for all the calls made during the whole week.
-# ## First, construct a data frame that represents the weighted relationships between nodes.
-# ## Each node is a caller and/or callee, whose location is mapped to a cell_id.
-# weightedEdges <- getWeightedEdges(my.call.data)
-# ## Reduce to only the locations that can be plotted
-# badIndices <- vector()
-# for(i in 1:nrow(weightedEdges)) {
-#   caller_loc <- toString(weightedEdges$caller_location[i])
-#   caller_rowIndex <- cell_id.coord.rowIndex[[caller_loc]]
-#   callee_loc <- toString(weightedEdges$callee_location[i])
-#   callee_rowIndex <- cell_id.coord.rowIndex[[callee_loc]]
-#   if(is.null(caller_rowIndex) || is.null(callee_rowIndex)) {
-#     badIndices <- c(badIndices, i)
-#   }
-# }
-# if(length(badIndices) > 0) { # remove the bad rows, if any
-#   weightedEdges <- weightedEdges[-badIndices, ]
-# }
-# 
-# ## Convert into cell location names compatible with the spreadsheet
-# caller_cell_loc <- vector()
-# callee_cell_loc <- vector()
-# for(i in 1:nrow(weightedEdges)) {
-#   caller_loc_str <- toString(weightedEdges$caller_location[i])
-#   rowIndex <- cell_id.coord.rowIndex[[caller_loc_str]]
-#   caller_cell_loc[i] <- cell.towers$Cell[rowIndex]
-#   
-#   callee_loc_str <- toString(weightedEdges$callee_location[i])
-#   rowIndex <- cell_id.coord.rowIndex[[callee_loc_str]]
-#   callee_cell_loc[i] <- cell.towers$Cell[rowIndex]
-# }
-# ## Merge with the weighted edges
-# weightedEdges$caller_cell_loc <- caller_cell_loc
-# weightedEdges$callee_cell_loc <- callee_cell_loc
-# 
-# ## Create an igraph object from the weighted edges -- from the last 2 columns only
-# ## (because those are the plottable locations that are compatible with the spreadsheet
-# ## "Cell" column.)
-# weightedEdges.matrix <- as.matrix(weightedEdges[, 6:7])
-# g <- graph.edgelist(weightedEdges.matrix)
-# E(g)$weight <- weightedEdges$Freq # add weights to the edges
-# 
-# ## Configure the graph for visualization
-# V(g)$size <- degree(g, mode = "all")
-# g <- decorate_graph(g, cell.towers, stratum = "Cell")
-# ## Retrieve the map from Google Maps
-# g.map <- get_map(location, maptype = "terrain", zoom = 10, scale=2)
-# g.map <- ggmap(g.map, extent = 'device', legend = 'none')
-# ## Add nodes to the map, where each code is described by a pair of coordinates
-# ## and scaled by its size (i.e., call frequency)
-# g.map <- g.map + geom_nodeset(aes(x = Longitude, y = Latitude, size=size),
-#                               graph=g, color="red")
-# ## Add edges to the map, where each edge is also scaled by pairwise frequency
-# g.map <- g.map + geom_edgeset(aes(x = Longitude, y = Latitude, size=weight),
-#                               graph=g, color="blue")
-# ## Add title and theme (optional)
-# g.map <- g.map + ggtitle("Spatial Call Graph for One-week Duration")
-# g.map <- g.map + fivethirtyeight_theme()
-# ## Disable any legend
-# g.map <- g.map + guides(fill=FALSE, alpha=FALSE, size=FALSE)
-# ## Display the map
-# print(g.map)
-# 
-# ## Save the plot to disk
-# ggsave(filename="./figures/mobile/my_call_graph.png", width=8, height=8)
+## Match each cell_id with its corresponding rowIndex of the coordinate table
+progress.bar <- create_progress_bar("text")
+progress.bar$init(nrow(call.data))
+rowIndices <- vector() # a vector of row indices
+nMatches <- 0 # count the number of matches
+for(i in 1:nrow(call.data)) {
+  ## Have to convert numeric to string in order to do lookup
+  cell_id <- toString(call.data$cell_id[i])
+  if(nchar(cell_id) > 0) {
+    ## Look up the (loaded) table
+    rowIndex <- cell_id.coord.rowIndex[[cell_id]]
+    if(is.null(rowIndex)) { # if matched
+      rowIndices <- c(rowIndices, NA)
+    } else { # if unmatched
+      rowIndices <- c(rowIndices, rowIndex)
+      nMatches <- nMatches + 1
+    }
+  } else {
+    rowIndices <- c(rowIndices, NA)
+  }
+  
+  progress.bar$step()
+}
+## Calculate the matched percentage
+match.pct <- round(nMatches / nrow(call.data) * 100, 2)
+print(paste("Percent matched locations =", match.pct))
+
+## Reduce call data to those that can be plotted on a map
+my.call.data <- call.data[!is.na(rowIndices), ]
+## Further reduce to include a certain date range only. Why?
+(table(my.call.data$date))
+my.call.data <- subset(my.call.data, date > 20080229)
+my.call.data <- subset(my.call.data, date < 20080308)
+
+## Aggregate all the call records by date and find the locations of the cell towers
+## and the frequency of the calls made from each.
+aggregate.location <- aggregateLocationByDate(my.call.data,
+                                              cell_id.coord.rowIndex, cell.towers)
+
+## Create a spatial bubble chart
+## First, need a location at the center of all cell towers in my call data
+## Calculate the mean longitude
+all.longitude <- names(table(aggregate.location$longitude))
+all.longitude <- as.numeric(all.longitude)
+mean.longitude <- mean(all.longitude)
+
+## Calculate the mean latitude
+all.latitude <- names(table(aggregate.location$latitude))
+all.latitude <- as.numeric(all.latitude)
+mean.latitude <- mean(all.latitude)
+
+## Retrieved a map centered at the given location
+location <- c(mean.longitude, mean.latitude)
+location.map <- get_map(location, maptype = "terrain", zoom = 10, scale=2)
+location.map <- ggmap(location.map, extent = 'device', legend = 'none')
+## Add the "bubbles" to the map, and size them by frequency.
+## Here we choose shape 21 (circle) and fill it red. For a full list of shapes,
+## see http://sape.inf.usi.ch/quick-reference/ggplot2/shape
+location.map <- location.map + geom_point(data = aggregate.location,
+                                          aes(x = longitude, y = latitude, size = freq),
+                                          fill="red", alpha=0.80, shape=21)
+## Remove any legends from the plot
+location.map <- location.map + guides(fill=FALSE, alpha=FALSE, size=FALSE)
+## Add a title to the plot
+location.map <- location.map + ggtitle("Distribution of Call Events over the Week")
+## Add a fancy theme to it (this step is optional)
+location.map <- location.map + fivethirtyeight_theme()
+## Split the plot into tiles (or facets), one for each date
+location.map <- location.map + facet_wrap(~date)
+## Display the plot (or else it won't show)
+print(location.map)
+
+## Save the plot on disk
+ggsave(filename="./figures/mobile/my_call_bubbles.png", width=10, height=10)
+
+## Here, we plot the population graph for all the calls made during the whole week.
+## First, construct a data frame that represents the weighted relationships between nodes.
+## Each node is a caller and/or callee, whose location is mapped to a cell_id.
+weightedEdges <- getWeightedEdges(my.call.data)
+## Reduce to only the locations that can be plotted
+badIndices <- vector()
+for(i in 1:nrow(weightedEdges)) {
+  caller_loc <- toString(weightedEdges$caller_loc[i])
+  caller_rowIndex <- cell_id.coord.rowIndex[[caller_loc]]
+  callee_loc <- toString(weightedEdges$callee_loc[i])
+  callee_rowIndex <- cell_id.coord.rowIndex[[callee_loc]]
+  if(is.null(caller_rowIndex) || is.null(callee_rowIndex)) {
+    badIndices <- c(badIndices, i)
+  }
+}
+if(length(badIndices) > 0) { # remove the bad rows, if any
+  weightedEdges <- weightedEdges[-badIndices, ]
+}
+
+## Convert into cell location names compatible with the spreadsheet
+caller_cell_loc <- vector()
+callee_cell_loc <- vector()
+for(i in 1:nrow(weightedEdges)) {
+  caller_loc_str <- toString(weightedEdges$caller_loc[i])
+  rowIndex <- cell_id.coord.rowIndex[[caller_loc_str]]
+  caller_cell_loc[i] <- cell.towers$Cell[rowIndex]
+  
+  callee_loc_str <- toString(weightedEdges$callee_loc[i])
+  rowIndex <- cell_id.coord.rowIndex[[callee_loc_str]]
+  callee_cell_loc[i] <- cell.towers$Cell[rowIndex]
+}
+## Merge with the weighted edges
+weightedEdges$caller_cell_loc <- caller_cell_loc
+weightedEdges$callee_cell_loc <- callee_cell_loc
+
+## Create an igraph object from the weighted edges -- from the last 2 columns only
+## (because those are the plottable locations that are compatible with the spreadsheet
+## "Cell" column.)
+ncol_weightedEdges <- ncol(weightedEdges)
+weightedEdges.matrix <- as.matrix(weightedEdges[, (ncol_weightedEdges-1):ncol_weightedEdges])
+g <- graph.edgelist(weightedEdges.matrix)
+E(g)$weight <- weightedEdges$freq # add weights to the edges
+
+## Configure the graph for visualization
+V(g)$size <- degree(g, mode = "all")
+g <- decorate_graph(g, cell.towers, stratum = "Cell")
+
+## Retrieve the map from Google Maps
+g.map <- get_map(location, maptype = "terrain", zoom = 11, scale=2)
+g.map <- ggmap(g.map, extent = 'device', legend = 'none')
+## Add nodes to the map, where each code is described by a pair of coordinates
+## and scaled by its size (i.e., call frequency)
+g.map <- g.map + geom_nodeset(aes(x = Longitude, y = Latitude, size=size),
+                              graph=g, color="red")
+## Add edges to the map, where each edge is also scaled by pairwise frequency
+g.map <- g.map + geom_edgeset(aes(x = Longitude, y = Latitude, size=weight),
+                              graph=g, color="blue")
+## Add title and theme (optional)
+g.map <- g.map + ggtitle("Spatial Call Graph for One-week Duration")
+g.map <- g.map + fivethirtyeight_theme()
+## Disable any legend
+g.map <- g.map + guides(fill=FALSE, alpha=FALSE, size=FALSE)
+## Display the map
+print(g.map)
+
+## Save the plot to disk
+ggsave(filename="./figures/mobile/my_call_graph.png", width=10, height=10)
