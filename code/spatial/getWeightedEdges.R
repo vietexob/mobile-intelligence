d2544de27b12b59850d06874ec507fd3273f4655
Viet Le Truc <vietexob@gmail.com>
2015-04-06 23:22:39 -0400

    To be continued

--- code/spatial/getWeightedEdges.R
+++ code/spatial/getWeightedEdges.R
@@ -18,9 +18,14 @@ getWeightedEdges <- function(call.data, lower.bound=0, upper.bound=50) {
     if(nrow(subset.caller_id) > 0) {
       ## Find the most frequented location
       cell_id.table <- table(subset.caller_id$cell_id)
-      most.freq.cell_id <- names(sort(cell_id.table, decreasing=TRUE))[1]
-#       print(paste(a_call_id, most.freq.cell_id))
-      caller_id.cell_id[[toString(a_call_id)]] <- most.freq.cell_id
+      cell_id.table <- subset(cell_id.table, cell_id.table > 0)
+#       print(length(table(cell_id.table)))
+#       most.freq.cell_id <- names(sort(cell_id.table, decreasing=TRUE))[1]
+      if(length(cell_id.table) > 1) {
+        print(a_call_id)
+        print(names(cell_id.table))
+      }
+      caller_id.cell_id[[toString(a_call_id)]] <- names(cell_id.table)
     }
   }
   
@@ -35,13 +40,19 @@ getWeightedEdges <- function(call.data, lower.bound=0, upper.bound=50) {
   ## Map each agent to a pair of spatial coordinates
   caller_location <- vector()
   callee_location <- vector()
+  caller_locs <- vector()
+  callee_locs <- vector()
   for(i in 1:nrow(weightedEdges)) {
     caller_id <- toString(weightedEdges$caller_id[i])
     caller_loc <- caller_id.cell_id[[caller_id]]
     if(is.null(caller_loc)) {
       caller_location[i] <- NA
     } else {
-      caller_location[i] <- caller_loc
+      if(length(caller_loc) == 1) {
+        caller_location[i] <- caller_loc
+      } else {
+        caller_locs <- caller_loc
+      }
     }
     
     callee_id <- toString(weightedEdges$callee_id[i])
@@ -49,7 +60,23 @@ getWeightedEdges <- function(call.data, lower.bound=0, upper.bound=50) {
     if(is.null(callee_loc)) {
       callee_location[i] <- NA
     } else {
-      callee_location[i] <- callee_loc
+      if(length(callee_loc) == 1) {
+        callee_location[i] <- callee_loc
+      } else {
+        callee_locs <- callee_loc
+      }
+    }
+    
+    if(length(caller_locs) > 0 || length(callee_locs) > 0) {
+      if(length(caller_locs) > 0 && length(callee_locs) > 0) {
+        
+      } else {
+        if(length(caller_locs) > 0) {
+          
+        } else {
+          print("This will never happen?")
+        }
+      }
     }
   }
   
